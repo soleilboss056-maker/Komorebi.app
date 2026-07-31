bf2a21ddadb3a72c7b9ebdc13a7fa05e72030d38
soleilboss056-maker <soleilboss056@gmail.com>
2026-07-31 20:24:57 +0100

    Update Discord bot invite URL to new client_id 1532724006535303238

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,2 @@
 export const INVITE_URL =
-  'https://discord.com/oauth2/authorize?client_id=1532402504174669855&permissions=8&integration_type=0&scope=bot'
+  'https://discord.com/oauth2/authorize?client_id=1532724006535303238&permissions=8&integration_type=0&scope=bot'
